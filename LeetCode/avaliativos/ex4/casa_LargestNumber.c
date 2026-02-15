be42e5f657d07d26d60a119453e801463d167b2c
Vinícius Lameirão <viniciuslameirao@hotmail.com>
2026-02-15 17:50:33 -0300

    doom style

--- LeetCode/avaliativos/ex4/casa_LargestNumber.c
+++ LeetCode/avaliativos/ex4/casa_LargestNumber.c
@@ -1,61 +1,76 @@
-int compare(char *a, char *b) {
-    char ab[22], ba[22];
-    strcpy(ab, a);
-    strcat(ab, b);
-    strcpy(ba, b);
-    strcat(ba, a);
-    return strcmp(ab, ba);
+int compare( char *a, char *b )
+{
+    char ab[22];
+    char ba[22];
+
+    strcpy( ab, a );
+    strcat( ab, b );
+
+    strcpy( ba, b );
+    strcat( ba, a );
+
+    return strcmp( ab, ba );
 }
 
-void quickSort(char **array, int iLeft, int iRight) {
-    int left = iLeft, right = iRight;
-    char *pivot = array[(left + right) / 2];
+void quickSort( char **array, int iLeft, int iRight )
+{
+    int     left;
+    int     right;
+    char    *pivot;
 
-    while (left <= right) {
-        while (compare(array[left], pivot) > 0)
+    left  = iLeft;
+    right = iRight;
+    pivot = array[( left + right ) / 2];
+
+    while ( left <= right )
+    {
+        while ( compare( array[left], pivot ) > 0 )
+        {
             left++;
-        while (compare(array[right], pivot) < 0)
+        }
+
+        while ( compare( array[right], pivot ) < 0 )
+        {
             right--;
-        if (left <= right) {
-            char *temp = array[left];
-            array[left] = array[right];
+        }
+
+        if ( left <= right )
+        {
+            char *temp;
+
+            temp         = array[left];
+            array[left]  = array[right];
             array[right] = temp;
+
             left++;
             right--;
         }
     }
 
-    if (iLeft < right)
-        quickSort(array, iLeft, right);
-
-    if (left < iRight)
-        quickSort(array, left, iRight);
-}
-
-char *largestNumber(int *nums, int numsSize) {
-    char **array = malloc(numsSize * sizeof(char *));
-
-    for (int i = 0; i < numsSize; i++) {
-        array[i] = malloc(11);
-        sprintf(array[i], "%d", nums[i]);
+    if ( iLeft < right )
+    {
+        quickSort( array, iLeft, right );
     }
 
-    quickSort(array, 0, numsSize - 1);
+    if ( left < iRight )
+    {
+        quickSort( array, left, iRight );
+    }
+}
 
-    if (array[0][0] == '0') {
-        char *res = malloc(2);
-        strcpy(res, "0");
-        return res;
+char *largestNumber( int *nums, int numsSize )
+{
+    char    **array;
+    char    *result;
+    int     totalLen;
+    int     i;
+
+    array = malloc( numsSize * sizeof( char * ) );
+
+    for ( i = 0; i < numsSize; i++ )
+    {
+        array[i] = malloc( 11 );
+        sprintf( array[i], "%d", nums[i] );
     }
 
-    int totalLen = 1;
-    for (int i = 0; i < numsSize; i++)
-        totalLen += strlen(array[i]);
-    char *result = malloc(totalLen);
-    result[0] = '\0';
-
-    for (int i = 0; i < numsSize; i++)
-        strcat(result, array[i]);
-
-    return result;
-}
+    quickSort( array, 0, nums
